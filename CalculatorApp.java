9b7b84ca86e502cbc1e1f9da5e21e4ec799bdb4e
Praveen Kulkarni <pkulkarni@zoura.com>
2021-06-27 18:31:09 +0530

    added file CalculatorApp.java from master

---
+++ CalculatorApp.java
@@ -0,0 +1,12 @@
+public class CalculatorApp{
+
+	public static void main(String[] args){
+
+		new CalculatorApp().addFromMaster(1,2);
+	}
+
+	public void addFromMaster(int a, int b){
+		return a+b;
+	}
+
+}
